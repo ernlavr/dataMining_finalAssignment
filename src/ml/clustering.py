73996094a8bfb671a2c9b71bcf48232ab3fae9c2
ErnestsLavrinovicsalias] <ernests.lavrinovics@gmail.com>
2023-12-17 18:10:31 +0100

    Merge simon-preprocessing

--- src/ml/clustering.py
+++ src/ml/clustering.py
@@ -1,28 +1,24 @@
-import pandas as pd
-import wandb
-import src.utils.utilities as utils
-import seaborn as sns
-import matplotlib.pyplot as plt
 import os
+
+import matplotlib.pyplot as plt
 import numpy as np
-
-from sklearn.cluster import DBSCAN, KMeans, OPTICS, AgglomerativeClustering
-from sklearn.neighbors import NearestNeighbors
-from sklearn.decomposition import PCA, FastICA
-from sklearn.manifold import TSNE
+import pandas as pd
+import seaborn as sns
 import sklearn.metrics as metrics
+from sklearn.cluster import DBSCAN, KMeans
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+from sklearn.neighbors import NearestNeighbors
 
 
-
-class Clustering():
+class Clustering:
     def __init__(self, args):
         self.save_dir = os.path.join(os.getcwd(), "output", "images", "Clustering")
         self.train = pd.read_csv(args.data_parsed)
         self.random_state = 42
 
-        # self.reduced_train = self.tsne_dimensionality_reduction(self.train, 3)
-        self.reduced_train = self.pcaDimensionalityReduction(self.train, 3, make_plots=True)
-        
+    def __call__(self) -> None:
+        self.reduced_train = self.pcaDimensionalityReduction(self.train, 3)
         self.perform_kmeans(self.reduced_train)
         self.perform_dbscan(self.reduced_train)
 
@@ -34,14 +30,10 @@ class Clustering():
         # Create a KMeans instance with k clusters: model
         model = KMeans(n_clusters=8, random_state=self.random_state)
 
-        # Fit model to samples
-        model.fit(dataset)
-
         # Determine the cluster labels of new_points: labels
-        labels = model.predict(dataset)
-        self.train['Cluster'] = labels
+        labels = model.fit_predict(dataset)
+        self.train["Cluster"] = labels
 
-        dataset = self.pcaDimensionalityReduction(dataset, 2)
         dataset['Cluster'] = labels
 
         # Add the cluster labels to your DataFrame
@@ -49,29 +41,28 @@ class Clustering():
         self.evaluate_clustering(self.train)
         self.plot_confusion(labels, self.train, "kmeans")
 
-
     def evaluate_clustering(self, dataset):
-        """ Evaluate the clustering using mutual information score """
+        """Evaluate the clustering using mutual information score"""
         # get the labels
         labels = self.train["account_type"]
 
         # get the cluster labels
         cluster_labels = dataset["Cluster"]
-        
 
         # calculate the mutual information score
         mutual_info = metrics.adjusted_mutual_info_score(labels, cluster_labels)
         homogeneity_score = metrics.homogeneity_score(labels, cluster_labels)
         completeness_score = metrics.completeness_score(labels, cluster_labels)
         silhouette_score = metrics.silhouette_score(dataset, dataset["Cluster"])
-        pair_confusion_matrix = metrics.cluster.pair_confusion_matrix(labels, cluster_labels)
+        pair_confusion_matrix = metrics.cluster.pair_confusion_matrix(
+            labels, cluster_labels
+        )
 
         # log to wandb
         print(f"Adj. Mutual information score: {round(mutual_info, 3)}")
         print(f"Homogeneity score: {round(homogeneity_score, 3)}")
         print(f"Completeness score: {round(completeness_score, 3)}")
         print(f"Silhouette score: {round(silhouette_score, 3)}")
-
 
     def collapse_to_two_clusters(self, labels):
         num_clusters = len(set(labels))
@@ -83,60 +74,61 @@ class Clustering():
         return collapsed_labels
 
     def perform_dbscan(self, dataset):
-        # TODO: balance human/bot labels
-        # numerical_columns = utils.get_numeric_columns()
-        # dataset = dataset[numerical_columns]
-
-        # Apply DBSCAN
-        # remove 'account_type'
+        # Drop target to prevent cheating
         dataset = dataset.drop(columns=['account_type'])
+
+        # Determine hyperparameters
         self.determine_dbscan_eps(dataset)
         ms = self.get_optimal_minsamples_dbscan(dataset)
+
+        # Cluster
         dbscan = DBSCAN(eps=0.02, min_samples=ms)
         clusters = dbscan.fit_predict(dataset)
 
-        dataset['Cluster'] = clusters
-        self.visualize(clusters, dataset, 'DBSCAN')
-
-        # plot confusion matrix between clusters and labels
-        sns.heatmap(pd.crosstab(clusters, self.train['account_type']), annot=True, fmt='d')
-        plt.xlabel('Labels')
-        plt.ylabel('Cluster')
+        # Visualize
+        dataset["Cluster"] = clusters
+        self.visualize(clusters, dataset, "DBSCAN")
+        sns.heatmap(
+            pd.crosstab(clusters, self.train["account_type"]), annot=True, fmt="d"
+        )
+        plt.xlabel("Labels")
+        plt.ylabel("Cluster")
+        plt.show()
         plt.savefig(os.path.join(self.save_dir, "DBSCAN", "confusion_matrix.png"))
         plt.clf()
         plt.close()
 
         # add clusters into self.train
-        self.train['Cluster'] = clusters
+        self.train["Cluster"] = clusters
         self.evaluate_clustering(self.train)
 
-        self.pcaDimensionalityReduction(dataset, 3)
-
         return dataset
-    
+
     def plot_confusion(self, clusters, dataset, name):
         # plot confusion matrix between clusters and labels
-        sns.heatmap(pd.crosstab(clusters, dataset['account_type']), annot=True, fmt='d')
-        plt.xlabel('Labels')
-        plt.ylabel('Cluster')
+
+        sns.heatmap(pd.crosstab(clusters, dataset["account_type"]), annot=True, fmt="d")
+        plt.xlabel("Labels")
+        plt.ylabel("Cluster")
+        plt.title("Confusion Matrix")
+        plt.show()
         plt.savefig(os.path.join(self.save_dir, name, f"{name}_conf_matrix.png"))
         plt.clf()
         plt.close()
 
     def determine_dbscan_eps(self, dataset):
-        """ Use K-Nearest Neighbors to determine the optimal eps value for DBSCAN
-         
-            https://www.aaai.org/Papers/KDD/1996/KDD96-037.pdf 
-            https://www.khoury.northeastern.edu/home/vip/teach/DMcourse/2_cluster_EM_mixt/notes_slides/revisitofrevisitDBSCAN.pdf
+        """Use K-Nearest Neighbors to determine the optimal eps value for DBSCAN
+        As per:
+        https://www.khoury.northeastern.edu/home/vip/teach/DMcourse/2_cluster_EM_mixt/notes_slides/revisitofrevisitDBSCAN.pdf
         """
-        k_neighbours = self.get_optimal_minsamples_dbscan(dataset) # same as minsamples
+        k_neighbours = self.get_optimal_minsamples_dbscan(dataset)  # same as minsamples
         neighbors = NearestNeighbors(n_neighbors=k_neighbours)
         neighbors_fit = neighbors.fit(dataset)
         distances, indices = neighbors_fit.kneighbors(dataset)
 
         distances = np.sort(distances, axis=0)
-        distances = distances[:,1]
-        
+        distances = distances[:, 1]
+
         # retrieve the best eps
         knee_point = self.find_knee_point(distances)
         best_eps = distances[knee_point]
@@ -144,7 +136,9 @@ class Clustering():
         # plot distances with marked knee point
         plt.plot(distances)
         plt.plot(1980, 0.02, 'ro', alpha=0.33)  # hard-coded to match final plot
-        plt.annotate('knee point', xy=(1980, 0.02), xytext=(1990, 0.02 + 0.01))
+        plt.annotate(
+            "knee point", xy=(1980, 0.02), xytext=(1990, 0.02 + 0.01)
+        )
         plt.xlabel('Data point index')
         plt.ylabel('Distance')
 
@@ -177,95 +171,70 @@ class Clustering():
         knee_point_index = np.argmax(dy)
 
         return knee_point_index
-        
 
     def get_optimal_minsamples_dbscan(self, dataset):
-        """ Return twice the number of features in the dataset
-        As per https://link.springer.com/article/10.1023/A:1009745219419 
-        """
-        return dataset.shape[1] * 2 + 10
-    
+        """Return twice the number of features in the dataset """
+        beta = 10 # empirical value
+        return dataset.shape[1] * 2 + beta
+
+
     def make_pair_plot(self, pca_results):
-        """ Make a pair plot of the PCA results """
+        """Make a pair plot of the PCA results"""
         # Plot the pairwise relationships between the principal components colored by the labels
         sns.pairplot(pca_results, hue='account_type')
-        plt.savefig(os.path.join(os.getcwd(), "output", "images", 'dimensionality', "pairplot.png"))
+        sns.pairplot(pca_results, hue="account_type")
+        save_dir = os.path.join(os.getcwd(), "output", "images", 'dimensionality')
+        os.makedirs(save_dir, exist_ok=True)
+        plt.savefig(os.path.join(save_dir, "pairplot.png"))
         plt.clf()
         plt.close()
-        
-    def pcaDimensionalityReduction(self, dataset, components, make_plots=False):
-        """ Reduce the dimensionality of the dataset using PCA """
+
+    def pcaDimensionalityReduction(self, dataset, components):
+        """Reduce the dimensionality of the dataset using PCA"""
         # remove "account_type" column
         if "account_type" in dataset.columns.values:
             dataset = dataset.drop(columns=["account_type"])
 
         pca = PCA(n_components=components)
-        pca_components = pca.fit_transform(dataset)
-        
+        pca_components = pca.fit_transform(dataset)       
         cols = [f'PC_{i}' for i in range(components)]
         output = pd.DataFrame(data=pca_components, columns=cols)
-        output['account_type'] = self.train['account_type']
+        output["account_type"] = self.train["account_type"]
 
-
-        if make_plots:
-            self.make_pair_plot(output)
-            # Plot the principal components
-            fig = plt.figure()
-            if components == 3:
-                ax = fig.add_subplot(111, projection='3d')
-                ax.scatter(output['PC_0'].values, output['PC_1'].values, output['PC_2'].values, alpha=0.25, c=output['account_type'].values)
-            else:
-                ax = fig.add_subplot(111)
-                ax.scatter(output['PC_0'].values, output['PC_1'].values, alpha=0.25)
-            plt.xlabel('PC_1')
-            plt.ylabel('PC_2')
-            ax.set_zlabel('PC_3')
-            plt.title('Data after PCA Transformation')
-            plt.show()
-            # plt.savefig(os.path.join(self.save_dir, "dimensionality", "PCA.png"))
-            plt.clf()
-            plt.close()
+        self.make_pair_plot(output)
+        # Plot the principal components
+        fig = plt.figure()
+        if components == 3:
+            ax = fig.add_subplot(111, projection='3d')
+            ax.scatter(output['PC_0'].values, output['PC_1'].values, output['PC_2'].values, alpha=0.25, c=output['account_type'].values)
+        else:
+            ax = fig.add_subplot(111)
+            ax.scatter(output['PC_0'].values, output['PC_1'].values, alpha=0.25)
+        plt.xlabel('PC_1')
+        plt.ylabel('PC_2')
+        ax.set_zlabel('PC_3')
+        plt.title('Data after PCA Transformation')
+        plt.show()
+        # plt.savefig(os.path.join(self.save_dir, "dimensionality", "PCA.png"))
+        plt.clf()
+        plt.close()
 
         return output
 
-    def tsne_dimensionality_reduction(self, dataset, components):
-        """ Reduce the dimensionality of the dataset using t-SNE """
-        # remove "account_type" column
-        if "account_type" in dataset.columns.values:
-            dataset = dataset.drop(columns=["account_type"])
-
-        for perplexity in [80]:    
-            tsne = TSNE(n_components=components, perplexity=perplexity, n_iter=5000)
-            tsne_components = tsne.fit_transform(dataset)
-            
-            cols = [f'Dim_{i}' for i in range(components)]
-            output = pd.DataFrame(data=tsne_components, columns=cols)
-            output['account_type'] = self.train['account_type'] # add labels back in
-
-            self.make_pair_plot(output)
-
-            # Plot the principal components
-            fig = plt.figure()
-            if components == 3:
-                ax = fig.add_subplot(111, projection='3d')
-                ax.scatter(output['Dim_0'].values, output['Dim_1'].values, output['Dim_2'].values, alpha=0.25)
-            else:
-                ax = fig.add_subplot(111)
-                ax.scatter(output['Dim_0'].values, output['Dim_1'].values, alpha=0.25)
-            plt.xlabel('PC_1')
-            plt.ylabel('PC_2')
-            plt.title('Data after t-SNE Transformation')
-            plt.show()
-            plt.clf()
-            plt.close()
-
-        return output
 
     def visualize(self, clusters, dataset, name, eps=None):
         # Visualize the clusters
         fig = plt.figure()
         ax = fig.add_subplot()
-        scatter = ax.scatter(dataset['PC_0'].values, dataset['PC_1'].values, c=clusters, cmap='viridis', marker='o', s=50, alpha=0.8)
+        scatter = ax.scatter(
+            dataset["PC_0"].values,
+            dataset["PC_1"].values,
+            c=clusters,
+            cmap="viridis",
+            marker="o",
+            s=50,
+            alpha=0.8,
+        )
         legend = ax.legend(*scatter.legend_elements(), title='Clusters')
         ax.add_artist(legend)
         ax.set_title(f'{name} Clustering; {eps}')
@@ -280,25 +249,8 @@ class Clustering():
         plt.clf()
         plt.close()
 
-        # With PCA plotting: X-Y axis is the top two principal components
-
-        # Take clusters and project them using either:
-            # U-Map
-            # T-SNE
-
-        # This gives X-clusters
-        # For each datapoint assign the Bot/Human label
-            # Ideally looks seperated
-            # If there's many, then there's different types of humans/bots
-
-        # Additionally:
-            # Mutual label scores
-            # Mutual information score (for evaluating clustering (train/val splits))
-            
-
     def compute_elbow(self):
-        """ Make an elbow plot """
-
+        """Make an elbow plot"""
         inertias = []
         for i in range(1, 20):
             # Create a KMeans instance with k clusters: model
@@ -316,14 +268,14 @@ class Clustering():
         # Get the best k
         best_k = 0
         for i in range(1, len(inertias)):
-            if abs(inertias[i] - inertias[i-1]) < 0.1:
+            if abs(inertias[i] - inertias[i - 1]) < 0.1:
                 best_k = i
                 break
 
         # Plot ks vs inertias
-        plt.plot(range(1, 20), inertias, '-o')
-        plt.xlabel('number of clusters, k')
-        plt.ylabel('Inter-cluster variance')
+        plt.plot(range(1, 20), inertias, "-o")
+        plt.xlabel("number of clusters, k")
+        plt.ylabel("Inter-cluster variance")
         plt.xticks(range(1, 20))
 
         # Save
--- src/utils/utilities.py
+++ src/utils/utilities.py
@@ -1,98 +1,49 @@
-import pandas as pd
-import argparse
-import omegaconf
 import os
 
-def get_data(path : str) -> pd.DataFrame:
+import pandas as pd
+
+
+def get_data(path: str) -> pd.DataFrame:
     output = pd.read_csv(path, encoding="ISO-8859-1")
     print(f"Loading data from {path}; Len: {output.shape[0]}")
     return output
 
-def getArgs() -> argparse.Namespace:
-    parser = argparse.ArgumentParser()
 
-    parser.add_argument("--args_path", type=str, default=None, help="Path to args file")
-    # Data
-    parser.add_argument("--humans_folder", type=str, default="data/e13", help="Path to humans folder")
-    parser.add_argument("--bots_folder", type=str, default="data/twt", help="Path to bots folder")
-    parser.add_argument("--data_train", type=str, default="data/processed/train.csv", help="Path to data_train")
-    parser.add_argument("--data_test", type=str, default="data/processed/test.csv", help="Path to data_test")
-    parser.add_argument("--data_parsed", type=str, default="data/processed/data_parsed.csv", help="Path to complete dataset")
-
-    # Preprocessing
-    parser.add_argument("--preprocess", type=bool, default=False, help="Preprocess data")
-
-    # Clustering
-    parser.add_argument("--cluster", type=bool, default=False, help="Cluster data")
-
-    # check if we have a config file
-    args = parser.parse_args()
-    if args.args_path is not None:
-        # load the config file
-        args = omegaconf.OmegaConf.load(args.args_path)
-        args = omegaconf.OmegaConf.to_container(args, resolve=True)
-        
-        # convert to args namespace
-        args = argparse.Namespace(**args)
-
-    return args
-
-def save_tmp_data(data : pd.DataFrame, name : str):
-    """ Save data to a temporary file """
+def save_tmp_data(data: pd.DataFrame, name: str):
+    """Save data to a temporary file"""
     output_dir = os.path.join(os.getcwd(), "tmp")
     os.makedirs(output_dir, exist_ok=True)
 
     data.to_csv(os.path.join(output_dir, name), index=False)
 
-def get_numeric_columns() -> list:
-    """ Get the numeric columns of the dataset """
-    numeric_columns = [
-        "statuses_count",
-        "followers_count",
-        "friends_count",
-        "favourites_count",
-        "listed_count",
-        "description_num_char",
-        "avg_retweets",
-        "avg_favorites",
-        "avg_length",
-        "median_day_of_tweeting", # -1 if no tweets
-        "median_time_of_tweeting", # -1 if no tweets
-    ]
-    return numeric_columns
 
 def get_categorical_columns() -> list:
     cat_columns = [
-        "account_type", # 0, 1; is human or bot?
+        "account_type",  # 0, 1; is human or bot?
         "url",  # 0, 1; is URL present?
-        "location", # 0, 1; is location present?
-        "default_profile", # 0, 1; is default profile present?
-        "default_profile_image", # 0, 1; is default profile image present?
-        "geo_enabled", # 0, 1; is geo enabled?
-        "profile_banner_url", # 0, 1; is profile banner URL present?
-        "profile_use_background_image", # 0, 1; is profile background image present?
-        "description_contins_link", # 0, 1; does description contain a link?
+        "location",  # 0, 1; is location present?
+        "default_profile",  # 0, 1; is default profile present?
+        "default_profile_image",  # 0, 1; is default profile image present?
+        "geo_enabled",  # 0, 1; is geo enabled?
+        "profile_banner_url",  # 0, 1; is profile banner URL present?
+        "profile_use_background_image",  # 0, 1; is profile background image present?
+        "description_contins_link",  # 0, 1; does description contain a link?
         "created_time_of_day",
-        "created_day_of_week"
+        "created_day_of_week",
     ]
     return cat_columns
 
+
 def get_date_feature_columns() -> list:
-    date_columns = [
-        "created_at"
-    ]
+    date_columns = ["created_at"]
     return date_columns
 
+
 def get_text_feature_columns() -> list:
-    text_columns = [
-        "description",
-        "screen_name"
-    ]
+    text_columns = ["description", "screen_name"]
     return text_columns
 
+
 def ignore_features_columns() -> list:
-    ignore_columns = [
-        "id",
-        "location"
-    ]
+    ignore_columns = ["id", "location"]
     return ignore_columns
